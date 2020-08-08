040311fed5a49303727ef47462924be884df3982
seunbayo <seunbayo83@gmail.com>
2020-08-08 14:28:50 +0100

    added needed files

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
+//adding body parser
 var bodyParser = require("body-parser"),
+//adding mongoose
 mongoose       = require("mongoose"),
+//adding express
 express = require("express"),
 app = express();
 
-mongoose.connect("mongodb://localhost/restful_blog_app")
+mongoose.connect("mongodb://localhost/restful_blog_app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+//Add files from views
+app.set("view engine", "ejs");
+//add files from public directory
+app.set(express.static("public"));
+//use body parser
+app.use(bodyParser.urlencoded({encoded: true}));
+
+
+
 // title
 // Image
 // body
 // created
+
+
+
+
+
+
+
+app.listen(3000, function() {
+    console.log('server is up');
+    
+})
