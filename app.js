4855568de83dbe3e2e1832ce87796f0efb14465f
seunbayo <seunbayo83@gmail.com>
2020-08-10 09:40:54 +0100

    added routes and a blog

--- app.js
+++ app.js
@@ -6,16 +6,45 @@ mongoose       = require("mongoose"),
 express = require("express"),
 app = express();
 
-mongoose.connect("mongodb://localhost/restful_blog_app", {
+//APP CONFIG
+mongoose.connect("mongodb://localhost/blog_app", {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+});
 //Add files from views
 app.set("view engine", "ejs");
 //add files from public directory
 app.set(express.static("public"));
 //use body parser
-app.use(bodyParser.urlencoded({encoded: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//MONGOOSE CONFIG
+//creating a schema
+var blogSchema = new mongoose.Schema ({
+    title: String,
+    image: String,
+    created: {type:Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema)
+
+// ROUTES
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+    //RETRIEVE ALL  BLOG FROM DATABASE
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log('ERROR');
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+    res.render("index");
+});
+
 
 
 
