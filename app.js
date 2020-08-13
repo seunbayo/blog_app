0332dfc820097cd62478cbbca6039c01221a2f10
seunbayo <seunbayo83@gmail.com>
2020-08-13 12:17:49 +0100

    added a delete route

--- app.js
+++ app.js
@@ -101,6 +101,22 @@ app.put("/blogs/:id", function (req, res){
   });
 });
 
+
+//DELETE ROUTE
+
+app.delete("/blogs/:id", function(req, res){
+  //destroy blog
+  Blog.findByIdAndRemove(req.params.id, function(err){
+    if(err){
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+});
+
+
+
 app.listen(3000, function () {
   console.log("server is up");
 });
